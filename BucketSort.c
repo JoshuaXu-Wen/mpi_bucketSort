882469069380325ed23e3a91bd9d59d8ad40fe8a
JoshuaXu-Wen <66362899+JoshuaXu-Wen@users.noreply.github.com>
2020-08-28 13:51:15 +1200

    Update BucketSort.c

--- BucketSort.c
+++ BucketSort.c
@@ -13,21 +13,13 @@ int cmpfunc (const void * a, const void * b) {
    return ( *(int*)a - *(int*)b );
 }
 
-// int * bucketInitialize( int *arr,  int length) {
-// 	int *newarr = (int*)malloc(length * sizeof(int));
-
-
-
-// }
-
 int main() {
+    
+    srand(12345);
 
-	srand(12345);
-
-	int i, j, k;
- 	//int counts[MAX_PROCESSES] = {0};
+    int i, j, k;
  	
-	MPI_Status Stat;
+    MPI_Status Stat;
     MPI_Init(NULL, NULL);
 
     // world_rank is process id
@@ -51,75 +43,27 @@ int main() {
     assert(final_num_per_proc != NULL);
 
     if(world_rank == 0) {
-    	
-    	//int counters = world_size;
-    	
 		longArr = (int*)malloc(SIZE * sizeof(int));
 		assert(longArr != NULL);
-		//displs = (int*)malloc(sizeof(int) * world_size);
-		//assert(displs != NULL);
-
-		// int j = 0;
-		// //displs[0] = 0;
-
-		// for(int i=0; i<SIZE; i++) {
-		// 	longArr[i] = rand();
-		// 	j = longArr[i] / Divide;
-		// 	counts[j]++;
-		// }
-		// printf("displs[0] is: %d\n", displs[0]);
-		// for(j=1; j<world_size; j++) {
-		// 	displs[j] = counts[j-1] + displs[j-1];
-		// 	printf("displs[%d] is: %d\n", j, displs[j]);
-		// }
 		printf("the original arrary is:\n");
 		for(int i=0; i<SIZE; i++) {
 			longArr[i] = rand();
 			printf("%d ", longArr[i]);
-			//j = longArr[i] / Divide;
-			//counts[j]++;
 		}
-
 		printf("\n\n");
-
-		// printf("the original counts is:\n");
-
-		// for(j=0; j<world_size; j++) {
-		// 	printf("%d \n", counts[j] );
-		// }	
-		// printf("\n\n");
-
-
 	}
 	MPI_Scatter(longArr, nums_per_proc, MPI_INT, bucketBuffer, 
 		nums_per_proc, MPI_INT, 0, MPI_COMM_WORLD);
 
-
-	// MPI_Scatter(longArr, 1, MPI_INT, &nums_per_proc, 
-	//  	1, MPI_INT, 0, MPI_COMM_WORLD);
-
-	//printf("the nums on process %d is: %d\n", world_rank, nums_per_proc);
-		//caculate the nums that will sent to each buckets
-
-
-
-	//if(world_rank == 0) {
-	// MPI_Scatterv(longArr, counts, displs, MPI_INT, bucketBuffer, 
-	// 		nums_per_proc, MPI_INT, 0, MPI_COMM_WORLD);
-	//}
-	//initial the small bucket
-	//int *bucket[world_size] = {NULL};
 	int *bucket = (int*)malloc(nums_per_proc * sizeof(int));
 	assert(bucket != NULL);
 	memset(bucket, 0, nums_per_proc * sizeof(int));
+	
 	//define the numbers in small buckets 
-	//int nums_per_small_bucket[world_size] = {0};
 	int * nums_per_small_bucket = (int*)malloc(world_size * sizeof(int));
 	assert(nums_per_small_bucket != NULL);
 	memset(nums_per_small_bucket, 0, world_size * sizeof(int));
-
-	//int index_per_small_bucket[world_size] = {0};  
-	//int recv_count[world_size] = {0};
+	//define received counter of each small buckets 
 	int * recv_count = (int*)malloc(world_size * sizeof(int));
 	assert(recv_count != NULL);
 	memset(recv_count, 0, world_size * sizeof(int));
@@ -150,24 +94,8 @@ int main() {
 		index[j]++;
 	}
 
-	// for (j=0; j<world_size; j++) {
-	// 	bucket[j] = (int *) calloc(nums_per_small_bucket[j], sizeof(int));
-	// }
-
-	// //scatter items from large buckets into small buckets
-	// for(i=0; i<nums_per_proc; i++) {
-	// 	j = bucketBuffer[i] / Divide;
-	// 	k = index[j];
-	// 	bucket[j][k] = bucketBuffer[i];
-	// 	index[j]++;
-	// }
-
-	//free(bucketBuffer);
-	//bucketBuffer = (int*)malloc(bucket[world_rank])
 	MPI_Alltoall(nums_per_small_bucket, 1, MPI_INT, recv_count, 1, MPI_INT, MPI_COMM_WORLD);
-	// initial the total numbers in the received large bucket
-	//int send_offset[world_size] = {0};
-	//int recv_offset[world_size] = {0};
+	// calculate the received offset of the received large bucket
 	int * recv_offset = (int*)malloc(world_size * sizeof(int));
 	assert(recv_offset != NULL);
 	memset(recv_offset, 0, world_size * sizeof(int));
@@ -180,7 +108,6 @@ int main() {
 	}
 	//the final large bucket on each proccessor
 	bucketBuffer = (int*)realloc(bucketBuffer, totalnums_received * sizeof(int));
-
 
 	MPI_Alltoallv(&bucket[0], &nums_per_small_bucket[0], &split[0], MPI_INT, 
 		bucketBuffer, &recv_count[0], &recv_offset[0], MPI_INT, MPI_COMM_WORLD);
